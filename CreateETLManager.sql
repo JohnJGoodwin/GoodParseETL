5ba611135b91d0277f7ec53c511c33188d428ee4
John Goodwin <51175644+JohnJGoodwin@users.noreply.github.com>
2019-06-06 12:16:46 -0400

    ETL Manager Database Script
    This script will;
    - Create an ETLManager database on your target MS SQL server.
    - Create an ETLManager table within the newly created ETLManager database on your chosen MS SQL server
    - Create a LoadConfig stored procedure within the newly created ETLManager database on your chosen MS SQL server
    
    To use this script, connect to your Microsoft SQL server of choice and execute the script.
    This script has been tested from within Visual Studio 2017 but should run equally well in Query Analyzer.
    
    This is an example project, thus this script is ridiculously simple. As a result it should be compatible with most older version MS SQL servers servers.

---
+++ CreateETLManager.sql
@@ -0,0 +1,46 @@
+﻿Create Database ETLManager;
+
+Go
+
+USE [ETLManager]
+GO
+
+/****** Object: Table [dbo].[ETLManager] Script Date: 6/6/2019 11:20:15 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ETLManager] (
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (50)  NOT NULL,
+    [FileLocation] NVARCHAR (MAX) NOT NULL,
+    [ParseType]    NVARCHAR (50)  NOT NULL,
+    [DestSQLDB]    NVARCHAR (50)  NOT NULL,
+    [DestSQLTable] NVARCHAR (50)  NOT NULL,
+    [ParseString]  NVARCHAR (MAX) NOT NULL,
+    [DestSQLProc]  NVARCHAR (50)  NOT NULL
+);
+
+Go
+
+CREATE PROCEDURE [dbo].[LoadConfig]
+	@ParseID int = 0
+
+AS
+	SELECT	[Name] 
+			,FileLocation 
+			,ParseType
+			,DestSQLDB
+			,DestSQLTable
+			,ParseString 
+			,DestSQLProc
+	FROM ETLManager
+	WHERE ID = @ParseID;
+RETURN 0
+
+Go
+
+
+
